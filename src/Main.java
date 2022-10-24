988973ba92f0cac234385c4d3ce9f4dc4c185dda
mertdogramaci <mertdogramaci@gmail.com>
2022-10-24 17:29:32 +0300

    added error handling

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        FileCipher fileCipher = new FileCipher(args);
+        if (args.length == 9) {
+            FileCipher fileCipher = new FileCipher(args);
+        } else {
+            System.out.println("You have typed the command-line argument as incomplete or it is completely empty!");
+        }
     }
 }
